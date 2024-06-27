5865f58ae5d882e35621ae1210283d2521835615
jnorm23 <joshua_norman11@hotmail.com>
2024-06-27 19:50:15 -0400

    answer

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -4,7 +4,14 @@ return a new string where the order of the characters is reversed.
 */
 
 // Your code here 
-
+function reverseString(word) {
+    let reversedWord = '';
+    
+    for (let i = word.length - 1; i >= 0; i--) {   
+        reversedWord += word[i];
+    }
+    return reversedWord;
+}
 // console.log(reverseString('fish')); // 'hsif'
 // console.log(reverseString('marathon')); // 'nohtaram'
 
