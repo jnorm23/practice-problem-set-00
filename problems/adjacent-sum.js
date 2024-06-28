4f6069605abff1c0b4685052471163a8d9e0fa16
jnorm23 <joshua_norman11@hotmail.com>
2024-06-27 20:22:25 -0400

    answer

--- problems/adjacent-sum.js
+++ problems/adjacent-sum.js
@@ -5,7 +5,14 @@ examples.
 */
 
 // Your code here 
-
+function adjacentSum(numbers) {
+    let sumArray = [];    
+    for (let i = 0; i < numbers.length - 1; i++) {
+        let sum = numbers[i] + numbers[i + 1];
+        sumArray.push(sum);
+    }    
+    return sumArray;
+}
 
 // console.log(adjacentSum([3, 7, 2, 11])); // [10, 9, 13], because [ 3+7, 7+2, 2+11 ]
 // console.log(adjacentSum([2, 5, 1, 9, 2, 4])); // [7, 6, 10, 11, 6], because [2+5, 5+1, 1+9, 9+2, 2+4]
